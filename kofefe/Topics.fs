1cc4ad2f3972d93a9de34f0a9a0d84934e8fcc3a
Gad Berger <gad.berger@gmail.com>
2020-07-10 08:42:31 -0400

    renaming timeout to timeoutInSeconds

--- kofefe/Topics.fs
+++ kofefe/Topics.fs
@@ -3,8 +3,8 @@
 open Confluent.Kafka
 
 module Topics =
-    let private timeout = 45.0
+    let private timeoutInSeconds = 45.0
 
     let listTopics (client:IAdminClient) =
-        let topics = client.GetMetadata(System.TimeSpan.FromSeconds(timeout))
+        let topics = client.GetMetadata(System.TimeSpan.FromSeconds(timeoutInSeconds))
         topics.Topics |> List.ofSeq |> List.map (fun x -> x.Topic)
