2ec94b51ece7662d379589893a823073fc08de41
Gad Berger <gad.berger@gmail.com>
2020-07-11 00:10:02 -0400

    fixing the consume function

--- kofefe/Topics.fs
+++ kofefe/Topics.fs
@@ -16,17 +16,18 @@ module Topics =
 
     let consume topic (consumer:IConsumer<string, string>) =
         let cts = new CancellationTokenSource()
+        let pos = 100
     
         // If all of the partitions are known, we can assign each partition an offset and read from that point
         // Otherwise, if this doesn't work, it might be required to get the latest offsets for each partition and subtract out
         // the requested length of items
-        let offset = TopicPartitionOffset(TopicPartition(topic, Partition(0)), Offset(-100L) )
+        let offset = TopicPartitionOffset(TopicPartition(topic, Partition(0)), Offset(-pos |> int64) )
         consumer.Assign([offset] |> List.toSeq)
     
-        // try to get 100 messages
+        // try to get offest number of messages
         let rec _consume i (messages:Message list) =
             match i with
-            | i when i = 100 -> messages
+            | i when i = pos -> messages
             | _ ->
                 let cr = consumer.Consume(cts.Token)
                 printfn "Consumed record with value %s" cr.Message.Value
@@ -39,6 +40,6 @@ module Topics =
                     Value = cr.Message.Value
                 }
 
-                message :: messages
+                _consume (i + 1) (message :: messages)
 
         _consume 0 []
